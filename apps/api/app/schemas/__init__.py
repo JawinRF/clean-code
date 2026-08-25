47ba25caf46740cebd038e9b9bf226cbb018b48c
JawinRF <fernandesjawin@gmail.com>
2026-08-25 07:52:14 +0530

    chore: export workspace schemas

--- apps/api/app/schemas/__init__.py
+++ apps/api/app/schemas/__init__.py
@@ -1,3 +1,9 @@
 from app.schemas.project import ProjectCreate, ProjectResponse
+from app.schemas.workspace import WorkspaceCreate, WorkspaceResponse
 
-__all__ = ["ProjectCreate", "ProjectResponse"]
+__all__ = [
+    "ProjectCreate",
+    "ProjectResponse",
+    "WorkspaceCreate",
+    "WorkspaceResponse",
+]
